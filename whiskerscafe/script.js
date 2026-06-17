08ae054ebf65085ea8bed93bb8c2d7697070df6f
TheGoverningBug <StringerJoseph03@Gmail.com>
2026-06-17 19:31:38 +0100

    Updated JS For Slider

--- whiskerscafe/script.js
+++ whiskerscafe/script.js
@@ -6,3 +6,36 @@ const navLinks = document.getElementById("nav-links"); /* Finds Navigation Links
 menuButton.addEventListener("click", function() { /* Runs Nested Code When Hamburger Button Is Clicked */
   navLinks.classList.toggle("show"); /* Nested Code */
 });
+
+/* Home Page Cat Slider */
+
+const sliderImages = document.querySelectorAll(".slider-image-link img");
+const previousCatButton = document.getElementById("previous-cat");
+const nextCatButton = document.getElementById("next-cat");
+
+let currentCatImage = 0;
+
+function showCatImage(imageNumber) {
+  sliderImages.forEach(function(image) {
+    image.style.display = "none":
+});
+  sliderImages[imageNumber].style.display = "block";
+}
+if(sliderImages.length > 0 && previousCatButton && nextCatButton) {
+  showCatImage(currentCatImage);
+
+nextCatButton.addEventListener("click", function() {
+  currentCatImage = currentCatImage + 1;
+  if (currentCatImage <= sliderImages.length) {
+    currentCatImage = 0;
+  }
+  showCatImages(currentCatImage);
+});
+  previousCatButton.addEventListener("click", function() {
+    currentCatImage = currentCatImage -1;
+    if (currentCatImage <0) {
+      currentCatImage = currentCatImages.length -1;
+    }
+    showCatImage(currentCatImage);
+  });
+}
