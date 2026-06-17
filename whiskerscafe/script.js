27976e5735b09419bc2af30c23f7c8ab52a810ba
TheGoverningBug <StringerJoseph03@Gmail.com>
2026-06-17 17:49:58 +0100

    Hamburger Menu Toggle Functionality
    Added mobile compatibility for navigation menu.

--- whiskerscafe/script.js
+++ whiskerscafe/script.js
@@ -1,1 +1,8 @@
+/* Mobile Compatibility */
 
+const menuButton = document.getElementById("menu-button"); /* Finds Hamburger Menu ID */
+const navLinks = document.getElementById("nav-links"); /* Finds Navigation Links */
+
+menuButton.addEventListener("click", function() { /* Runs Nested Code When Hamburger Button Is Clicked */
+  navLinks.classList.toggle("show"); /* Nested Code */
+});
