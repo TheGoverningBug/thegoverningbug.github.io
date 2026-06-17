9abeaba8b7264c9e4268f262097e8684395939d7
TheGoverningBug <StringerJoseph03@Gmail.com>
2026-06-17 17:53:55 +0100

    Clarify comment for menu button in script.js
    Updated comment for clarity regarding menu button.

--- whiskerscafe/script.js
+++ whiskerscafe/script.js
@@ -1,6 +1,6 @@
 /* Mobile Compatibility */
 
-const menuButton = document.getElementById("menu-button"); /* Finds Hamburger Menu ID */
+const menuButton = document.getElementById("menu-button"); /* Finds Hamburger Menu */
 const navLinks = document.getElementById("nav-links"); /* Finds Navigation Links */
 
 menuButton.addEventListener("click", function() { /* Runs Nested Code When Hamburger Button Is Clicked */
